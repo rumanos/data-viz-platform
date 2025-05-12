a746aaa7d7595604f4621b047c94867af917532b
Ruman <ruman.leo@gmail.com>
2025-05-12 22:11:56 +0530

    chore: setup vite react, typescript, tailwindcss, shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <h1>Start of something amazing!!!</h1>
+    </div>
+  )
+}
+
+export default App
