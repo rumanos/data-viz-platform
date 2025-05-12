daecdfd912372b065d04782be60b5396e733ed66
Ruman <ruman.leo@gmail.com>
2025-05-13 00:03:06 +0530

    feat: ProtectedRoute for secure pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import Loader from './components/ui/loader';
+import ProtectedRoute from './components/ProtectedRoute';
 const AuthPage = lazy(() => import('./pages/AuthPage'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const NotFound = lazy(() => import('./pages/NotFound'));
@@ -17,7 +18,9 @@ function App() {
           <Route path="/reset-password" element={<AuthPage />} />
           <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path=":menuKey" element={
+            <ProtectedRoute>
               <Dashboard />
+            </ProtectedRoute>
           } />
           <Route path="*" element={<NotFound />} />
         </Routes>
