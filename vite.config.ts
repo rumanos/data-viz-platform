b98f97e4154b9e0f1df21ae76fc83670be1049d4
Ruman <ruman.leo@gmail.com>
2025-05-13 02:25:27 +0530

    fix: configure tailwindcss to resolve rendering issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 /// <reference types="vitest" />
 import { defineConfig as defineVitestConfig } from 'vitest/config'
 import { mergeConfig } from 'vite'
@@ -8,7 +9,7 @@ import { mergeConfig } from 'vite'
 // https://vitejs.dev/config/
 // Merge Vite and Vitest configurations
 const viteConfig = defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
